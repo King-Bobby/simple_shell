52d8541b97447f2744e21e16e86040d1f780195a
Noel Brown <noelbrowndboss@gmail.com>
2023-02-24 13:54:51 -0800

    removed fork process

--- main.c
+++ main.c
@@ -9,9 +9,9 @@ int main(void)
 	char *command = NULL, *command_copy = NULL, *token;
 	char **argv;
 	size_t n = 0;
+	ssize_t num_chars;
 	const char *delim = " \n";
-	pid_t my_pid;
-	int num_chars, num_tokens, status, i;
+	int num_tokens, i;
 
 	while (1)
 	{
@@ -22,12 +22,15 @@ int main(void)
 			putchar('\n');
 			return (-1);
 		}
+
 		command_copy = malloc(sizeof(char) * num_chars);
 		strcpy(command_copy, command);
+		
 		token = strtok(command, delim);
 		for (num_tokens = 0; token != NULL; num_tokens++)
 			token = strtok(NULL, delim);
 		num_tokens++;
+		
 		argv =  malloc(sizeof(char *) * num_tokens);
 		token = strtok(command_copy, delim);
 		for (i = 0; token != NULL; i++)
@@ -37,11 +40,8 @@ int main(void)
 			token = strtok(NULL, delim);
 		}
 		argv[i] = NULL;
-		my_pid = fork();
-		if (my_pid == 0)
-			execmd(argv);
-		else
-			wait(&status);
+		
+		execmd(argv);
 	}
 	free(command_copy);
 	free(command);
